7ca5f81e834315b86d9a94c81a91ee7406ced57b
zakysf <zakysuryaf.999@gmail.com>
2025-05-21 23:38:21 +0700

    nambah dikit

--- login.php
+++ login.php
@@ -1,3 +1,31 @@
+<?php
+session_start();
+include "koneksi.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $konek->prepare("SELECT * FROM admin WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['admin'] = $username;
+            header("Location: indeks.html");
+            exit();
+        } else {
+            $error = "Password salah.";
+        }
+    } else {
+        $error = "Username tidak ditemukan.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,14 +39,14 @@
     <div class="login-container">
         <div class="form-box">
             <h2>Login</h2>
-            <form>
+            <form method="POST">
                 <div class="input-box">
                     <i class="fa fa-user"></i>
-                    <input type="text" placeholder="Username" required>
+                    <input type="text" name="username" placeholder="Username" required>
                 </div>
                 <div class="input-box">
                     <i class="fa fa-lock"></i>
-                    <input type="password" placeholder="Password" required>
+                    <input type="password" name="password" placeholder="Password" required>
                 </div>
                 <button type="submit" class="login-btn">Login</button>
                 <p class="signup-text">Don't have an account? <a href="#">Sign Up</a></p>
